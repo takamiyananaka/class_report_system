168d637951cee25dd438beb2207bc429c204ebff
Nanaka <1377918740@qq.com>
2025-11-11 13:49:08 +0800

    初始化

---
+++ src/main/java/com/nanaka/maven/class_report_system/ClassReportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nanaka.maven.class_report_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassReportSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassReportSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nanaka/maven/class_report_system/ClassReportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanaka.maven.class_report_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassReportSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
