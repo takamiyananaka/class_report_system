96d897ad6cc85b9a42c16bdbe385babee2cc2fad
Nanaka <1377918740@qq.com>
2025-12-09 17:17:00 +0800

    修复文档

--- src/main/java/com/xuegongbu/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/xuegongbu/common/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.xuegongbu.common.exception;
 
 import com.xuegongbu.common.Result;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Hidden
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
