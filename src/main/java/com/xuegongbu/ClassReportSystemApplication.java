f8c8555f6e8086ea661c3d938d9bc5eee22f190f
fengke <3233837659@qq.com>
2025-12-15 15:37:06 +0800

    fix:启动类配置

--- src/main/java/com/xuegongbu/ClassReportSystemApplication.java
+++ src/main/java/com/xuegongbu/ClassReportSystemApplication.java
@@ -2,8 +2,12 @@ package com.xuegongbu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableRetry
 public class ClassReportSystemApplication {
 
     public static void main(String[] args) {
