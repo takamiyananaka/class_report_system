da115e81060e2302d51ec7d1ce73134154a37e05
fengke <3233837659@qq.com>
2025-12-16 20:33:58 +0800

    fix；修复预警生成

--- src/main/java/com/xuegongbu/service/impl/AlertServiceImpl.java
+++ src/main/java/com/xuegongbu/service/impl/AlertServiceImpl.java
@@ -126,6 +126,7 @@ public class AlertServiceImpl extends ServiceImpl<AlertMapper, Alert> implements
                         attendance.getExpectedCount(),
                         attendanceRate.multiply(BigDecimal.valueOf(100)).doubleValue()));
                 alert.setReadStatus(0);
+                alert.setNotifyStatus(0);
                 save(alert);
                 
                 // 直接发送邮件通知
