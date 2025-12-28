5540e9104dcaa63b98dbe49ad138bdb3ea5c3f85
fengke <3233837659@qq.com>
2025-12-28 13:54:39 +0800

    fix:遗漏的提交

--- src/main/java/com/xuegongbu/dto/CollegeRequest.java
+++ src/main/java/com/xuegongbu/dto/CollegeRequest.java
@@ -23,25 +23,17 @@ public class CollegeRequest implements Serializable {
     private String name;
 
     /**
-     * 学院账号
-     */
-    @NotBlank(message="[学院账号]不能为空")
-    @Size(max= 50, message="学院账号长度不能超过50")
-    @Schema(description = "学院账号")
-    private String username;
-
-    /**
-     * 学院密码
-     */
-    @Size(max= 255, message="密码长度不能超过255")
-    @Schema(description = "学院密码")
-    private String password;
-
-    /**
      * 学院号
      */
     @NotBlank(message="[学院号]不能为空")
     @Size(max= 50, message="学院号长度不能超过50")
     @Schema(description = "学院号")
     private String collegeNo;
+
+    /**
+     * 学院描述
+     */
+    @Size(max= 500,message="编码长度不能超过500")
+    @Schema(description = "学院描述")
+    private String description;
 }
