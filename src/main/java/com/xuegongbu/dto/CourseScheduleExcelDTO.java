649f8232a642ca893e8b9bd46b702c9a4d19ca49
fengke <3233837659@qq.com>
2025-12-29 18:24:01 +0800

    fix:修改模板表头名

--- src/main/java/com/xuegongbu/dto/CourseScheduleExcelDTO.java
+++ src/main/java/com/xuegongbu/dto/CourseScheduleExcelDTO.java
@@ -14,55 +14,55 @@ public class CourseScheduleExcelDTO implements Serializable {
     /**
      * 课程名称
      */
-    @ExcelProperty(index = 0, value = "课程名称")
+    @ExcelProperty(index = 0, value = "KCM")
     private String courseName;
 
     /**
      * 课程号
      */
-    @ExcelProperty(index = 1, value = "课程号")
+    @ExcelProperty(index = 1, value = "KCH")
     private String courseNo;
 
     /**
      * 课序号
      */
-    @ExcelProperty(index = 2, value = "课序号")
+    @ExcelProperty(index = 2, value = "KXH")
     private String orderNo;
     
 
     /**
      * 星期几（汉字）
      */
-    @ExcelProperty(index = 3, value = "星期几")
+    @ExcelProperty(index = 3, value = "SKXQ")
     private String weekday;
 
     /**
      * 周次范围（格式：x-x周，例如：3-16周）
      */
-    @ExcelProperty(index = 5, value = "周次范围")
+    @ExcelProperty(index = 4, value = "ZCMC")
     private String weekRange;
 
     /**
      * 开始节次（1-12）
      */
-    @ExcelProperty(index = 6, value = "开始节次")
+    @ExcelProperty(index = 5, value = "KSJC")
     private Integer startPeriod;
 
     /**
      * 结束节次（1-12）
      */
-    @ExcelProperty(index = 7, value = "结束节次")
+    @ExcelProperty(index = 6, value = "JSJC")
     private Integer endPeriod;
 
     /**
      * 教室
      */
-    @ExcelProperty(index = 8, value = "教室")
+    @ExcelProperty(index = 7, value = "JASMC")
     private String classroom;
     
     /**
      * 上课班级列表（以,分割）
      */
-    @ExcelProperty(index = 9, value = "上课班级")
+    @ExcelProperty(index = 8, value = "WZSKBJ")
     private String classList;
 }
