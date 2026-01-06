5d9552ab838a86117a3454be0ea37c9db5b00f33
fengke <3233837659@qq.com>
2026-01-06 16:00:42 +0800

    fix:修复bug

--- src/main/java/com/xuegongbu/mapper/TeacherMapper.java
+++ src/main/java/com/xuegongbu/mapper/TeacherMapper.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface TeacherMapper extends BaseMapper<Teacher> {
 
     //通过学院名称查询教师工号(子查询获得学院号，通过学院号查)
-    @Select( "SELECT teacher_no FROM teacher WHERE college_no = (SELECT college_no FROM college WHERE college_name = #{collegeName})")
+    @Select( "SELECT teacher_no FROM teacher WHERE college_no = (SELECT college_no FROM college WHERE name = #{collegeName})")
      List<String> queryTeacherNoByCollegeName(@Param("collegeName") String collegeName);
 
 
