3b1588dbf4f3056bda4b63abf4a761b0e2e81510
fengke <3233837659@qq.com>
2026-01-19 14:34:29 +0800

    feat:图表生成加入默认查询参数

--- src/main/java/com/xuegongbu/dto/AttendanceChartWithClassDTO.java
+++ src/main/java/com/xuegongbu/dto/AttendanceChartWithClassDTO.java
@@ -8,19 +8,19 @@ import lombok.Data;
 @Schema(description = "考勤图表数据(班级)")
 public class AttendanceChartWithClassDTO {
 
-    @Schema(description = "学院名称")
+    @Schema(description = "学院名称",example = "计算机与软件学院")
     private String collegeName;
 
-    @Schema(description = "辅导员名字")
+    @Schema(description = "辅导员名字",example = "张老师")
     private String teacherName;
 
-    @Schema(description = "班级名称")
+    @Schema(description = "班级名称",example = "24计科4班")
     private String className;
 
-    @Schema(description = "精细度：1-按日，2-按周，3-按月，4-按学期")
+    @Schema(description = "精细度：1-按日，2-按周，3-按月，4-按学期",example = "1")
     @NotNull(message = "请选择精细度")
     private Integer granularity; // 1-daily, 2-weekly, 3-monthly, 4-termly
 
-    @Schema(description = "学期名称")
+    @Schema(description = "学期名称",example = "2025-2026学年秋季学期")
     private String semesterName;
 }
--- src/main/java/com/xuegongbu/dto/AttendanceChartWithCourseDTO.java
+++ src/main/java/com/xuegongbu/dto/AttendanceChartWithCourseDTO.java
@@ -7,16 +7,16 @@ import lombok.Data;
 @Data
 @Schema(description = "考勤图表查询参数（课程）")
 public class AttendanceChartWithCourseDTO {
-    @Schema(description = "任课教师名字")
+    @Schema(description = "任课教师名字",example = "李副教授")
     private String teacherName;
 
-    @Schema(description = "课序号")
+    @Schema(description = "课序号",example = "01")
     private String orderNo;
 
-    @Schema(description = "学期名称")
+    @Schema(description = "学期名称",example = "2025-2026学年秋季学期")
     private String semesterName;
 
-    @Schema(description = "精细度：1-按日，2-按周，3-按月，4-按学期")
+    @Schema(description = "精细度：1-按日，2-按周，3-按月，4-按学期",example = "1")
     @NotNull(message = "请选择精细度")
     private Integer granularity; // 1-daily, 2-weekly, 3-monthly, 4-termly
 }
