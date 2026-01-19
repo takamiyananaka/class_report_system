5005050e7aa314126f4016ad484b81745461dfd0
fengke <3233837659@qq.com>
2026-01-19 09:26:53 +0800

    fix:修复查询参数

--- src/main/java/com/xuegongbu/dto/AttendanceChartWithCourseDTO.java
+++ src/main/java/com/xuegongbu/dto/AttendanceChartWithCourseDTO.java
@@ -1,20 +1,20 @@
 package com.xuegongbu.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 
+@Data
+@Schema(description = "考勤图表查询参数（课程）")
 public class AttendanceChartWithCourseDTO {
-    @Schema(description = "学院名称")
-    private String collegeName;
-
-    @Schema(description = "辅导员名字")
+    @Schema(description = "任课教师名字")
     private String teacherName;
 
-    @Schema(description = "班级名称")
-    private String className;
-
-    @Schema(description = "精细度：1-按日，2-按周，3-按月，4-按学期")
-    private Integer granularity; // 1-daily, 2-weekly, 3-monthly, 4-termly
+    @Schema(description = "课序号")
+    private String orderNo;
 
     @Schema(description = "学期名称")
     private String semesterName;
+
+    @Schema(description = "精细度：1-按日，2-按周，3-按月，4-按学期")
+    private Integer granularity; // 1-daily, 2-weekly, 3-monthly, 4-termly
 }
